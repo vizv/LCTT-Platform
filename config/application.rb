e5017b232ebca404514e6c2cad04ddf30ceb7329
Wenxuan Zhao <vince.zhaowenxuan@gmail.com>
2014-06-23 01:14:24 -0300

    disable locales warning

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+# disable locales warning
+I18n.enforce_available_locales = false
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
